6f9b24a748e669eced0d61d829fc2e6bc74d944b
Willian Silva <williantafsilva@users.noreply.github.com>
2025-06-05 16:40:47 +0200

    Create maplist-fromto-stdout.sh

---
+++ maplist-fromto-stdout.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+############################################################################
+############################# STDOUT FUNCTION ##############################
+############################################################################
+##Author: Willian T.A.F. Silva (willian.silva@evobiolab.com).
+############################################################################
+##SCRIPT DESCRIPTION:
+
+##Description:
+##Select columns of a matrix in a specific order.
+
+##Input $1: Target list file.
+##Input $2: Map file with tab-separated lists of old strings and replacement strings (OLD\tNEW).
+##Output: Print new list.
+
+##Usage: 
+##maplist-fromto-stdout.sh <TARGET LIST FILE> <MAP FILE>
+
+############################################################################
+##ACTIONS:
+
+##Input.
+
+INPUTLIST=$1
+FROMTOLIST=$2
+
+##Process.
+
+awk -v OFS='\t' '
+NR == FNR {
+   map[$1] = $2
+   next
+}
+{
+   for (i=1; i<=NF; ++i)
+      $i in map && $i = map[$i]
+}
+1' ${FROMTOLIST} ${INPUTLIST}
+
