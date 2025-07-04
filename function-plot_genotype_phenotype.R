c7f5e4cc3ea5a6eeadce72ffaf486bf362034666
Willian Silva <williantafsilva@users.noreply.github.com>
2025-07-04 14:05:37 +0200

    Update function-plot_genotype_phenotype.R

--- function-plot_genotype_phenotype.R
+++ function-plot_genotype_phenotype.R
@@ -5,9 +5,9 @@
 ############################################################################
 
 plot_genotype_phenotype<-function(MATRIX_GENOTYPES, #Data frame of genotypes with SNPs per row (1st column is SNP ID) and samples per column.
-                                  MATRIX_PHENOTYPES, #Data frame of phenotypic values with gene per row (1st column is gene ID) and samples per column.
+                                  MATRIX_PHENOTYPES, #Data frame of phenotypic values with phenotype per row (1st column is phenotype ID) and samples per column.
                                   VECTOR_SNP_ID, #Vector of SNPs to be plotted (up to 25).
-                                  VECTOR_GENE_ID, #Vector of genes to be plotted (up to 25).
+                                  VECTOR_PHENOTYPE_ID, #Vector of phenotypes to be plotted (up to 25).
                                   MINGENFREQ=0, #Minimum genotype frequency (<1) or count (>1).
                                   PLOTTITLE="Genotype x Phenotype",
                                   XLABEL="Genotype",
@@ -23,7 +23,7 @@ plot_genotype_phenotype<-function(MATRIX_GENOTYPES, #Data frame of genotypes wit
   DATA_GENOTYPES<-MATRIX_GENOTYPES[MATRIX_GENOTYPES[,1] %in% VECTOR_SNP_ID,]
   
   #Subset phenotype data.
-  DATA_PHENOTYPES<-MATRIX_PHENOTYPES[MATRIX_PHENOTYPES[,1] %in% VECTOR_GENE_ID,]
+  DATA_PHENOTYPES<-MATRIX_PHENOTYPES[MATRIX_PHENOTYPES[,1] %in% VECTOR_PHENOTYPE_ID,]
   
   #Transform phased genotypes into unphased genotypes.
   DATA_GENOTYPES<-data.frame(lapply(DATA_GENOTYPES,function(x){
@@ -73,32 +73,32 @@ plot_genotype_phenotype<-function(MATRIX_GENOTYPES, #Data frame of genotypes wit
   DATA_GENOTYPES<-DATA_GENOTYPES %>%
     filter(!(DATA_GENOTYPES[,1] %in% SNPltMINGENFREQ))
   
-  #Data frame with target SNP-gene pairs.
-  SNP_GENE<-data.frame(SNP_ID=VECTOR_SNP_ID,
-                       GENE_ID=VECTOR_GENE_ID)
-  SNP_GENE<-SNP_GENE %>%
+  #Data frame with target SNP-phenotype pairs.
+  SNP_PHENOTYPE<-data.frame(SNP_ID=VECTOR_SNP_ID,
+                            PHENOTYPE_ID=VECTOR_PHENOTYPE_ID)
+  SNP_PHENOTYPE<-SNP_PHENOTYPE %>%
     filter(!(SNP_ID %in% SNPltMINGENFREQ))
   
-  if(nrow(SNP_GENE)>MAXNPLOTS){
-    SNP_GENE<-SNP_GENE[1:min(MAXNPLOTS,nrow(SNP_GENE)),]
+  if(nrow(SNP_PHENOTYPE)>MAXNPLOTS){
+    SNP_PHENOTYPE<-SNP_PHENOTYPE[1:min(MAXNPLOTS,nrow(SNP_PHENOTYPE)),]
   }
   
   #Create data frame with genotypes and phenotypes.
   DATA_GENOTYPE_PHENOTYPE<-data.frame(SNP=NA,
-                                      GENE=NA,
+                                      PHENOTYPE=NA,
                                       SAMPLE=NA,
                                       GENOTYPE=NA,
-                                      PHENOTYPE=NA)
+                                      PHENOTYPEVALUE=NA)
   
-  for(i in 1:nrow(SNP_GENE)){
-    TMP<-data.frame(SNP=SNP_GENE$SNP_ID[i],
-                    GENE=SNP_GENE$GENE_ID[i],
+  for(i in 1:nrow(SNP_PHENOTYPE)){
+    TMP<-data.frame(SNP=SNP_PHENOTYPE$SNP_ID[i],
+                    PHENOTYPE=SNP_PHENOTYPE$PHENOTYPE_ID[i],
                     SAMPLE=colnames(DATA_GENOTYPES)[2:ncol(DATA_GENOTYPES)],
                     GENOTYPE=unname(unlist(DATA_GENOTYPES[
-                      DATA_GENOTYPES[,1]==SNP_GENE$SNP_ID[i],
+                      DATA_GENOTYPES[,1]==SNP_PHENOTYPE$SNP_ID[i],
                       2:ncol(DATA_GENOTYPES)])),
-                    PHENOTYPE=as.numeric(unname(unlist(DATA_PHENOTYPES[
-                      DATA_PHENOTYPES[,1]==SNP_GENE$GENE_ID[i],
+                    PHENOTYPEVALUE=as.numeric(unname(unlist(DATA_PHENOTYPES[
+                      DATA_PHENOTYPES[,1]==SNP_PHENOTYPE$PHENOTYPE_ID[i],
                       2:ncol(DATA_PHENOTYPES)]))))
     DATA_GENOTYPE_PHENOTYPE<-rbind(DATA_GENOTYPE_PHENOTYPE,TMP)
   }
@@ -106,15 +106,15 @@ plot_genotype_phenotype<-function(MATRIX_GENOTYPES, #Data frame of genotypes wit
   
   #Create plot.
   p.genxphen<-DATA_GENOTYPE_PHENOTYPE %>%
-    ggplot(aes(x=GENOTYPE,y=PHENOTYPE))+ 
+    ggplot(aes(x=GENOTYPE,y=PHENOTYPEVALUE))+ 
     geom_jitter(color="black",shape=20,size=0.2,
                 height=0,width=0.3)+
     geom_boxplot(aes(fill=GENOTYPE),
                  width=0.5,color="black",alpha=0.7,
                  outlier.color="black",outlier.shape=20,outlier.size=0.2)+
     labs(x=XLABEL,y=YLABEL)+
-    facet_wrap(~SNP+GENE,
-               nrow=floor(sqrt(nrow(SNP_GENE))),
+    facet_wrap(~SNP+PHENOTYPE,
+               nrow=floor(sqrt(nrow(SNP_PHENOTYPE))),
                scales="free")+
     ggtitle(PLOTTITLE)+
     myggplottheme_blank_nolegend
