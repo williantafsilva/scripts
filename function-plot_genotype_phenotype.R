d9989b7b8e5ed45d663b96e797ee7e5d5e85fd74
Willian Silva <williantafsilva@users.noreply.github.com>
2025-07-29 21:17:37 +0200

    Update function-plot_genotype_phenotype.R

--- function-plot_genotype_phenotype.R
+++ function-plot_genotype_phenotype.R
@@ -19,6 +19,23 @@ plot_genotype_phenotype<-function(MATRIX_GENOTYPES, #Data frame of genotypes wit
   library(tidyverse)
   library(gtools)
   
+  myggplottheme_blank_nolegend<-theme(title=element_text(size=10,face="bold"),
+                                      axis.title=element_text(size=10,face="bold"),
+                                      axis.text=element_text(size=10),
+                                      axis.text.x=element_text(angle=60,size=8,vjust=0.5),
+                                      legend.position="none",
+                                      #legend.title=element_text(size=10,face="bold"),
+                                      #legend.text=element_text(size=10),
+                                      #legend.key=element_blank(),
+                                      panel.grid=element_line(colour="gray90"),
+                                      panel.grid.major.x=element_blank(),
+                                      panel.grid.minor.x=element_blank(),
+                                      panel.background=element_rect(fill="white",colour="black"),
+                                      panel.grid.major=element_blank(),
+                                      panel.grid.minor=element_blank(),
+                                      strip.background=element_rect(colour="black",
+                                                                    fill="white"))
+  
   #Subset genotype data.
   DATA_GENOTYPES<-MATRIX_GENOTYPES[MATRIX_GENOTYPES[,1] %in% VECTOR_SNP_ID,]
   
