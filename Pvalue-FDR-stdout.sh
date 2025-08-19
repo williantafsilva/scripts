6702082ea3241bd15ddca4122d8ab76357397205
Willian Silva <williantafsilva@users.noreply.github.com>
2025-08-19 10:55:53 +0200

    Update Pvalue-FDR-stdout.sh

--- Pvalue-FDR-stdout.sh
+++ Pvalue-FDR-stdout.sh
@@ -12,10 +12,11 @@
 
 #Input $1: Tab-separated file with header containing a column with p-values.
 #Input $2: Index of column that contains p-values.
+#Input $3 (optional): Total number of tests (when only significant p-values are provided).
 #Output: FDR p-values.
 
 ##Usage: 
-##Pvalue-FDR-stdout.sh <INPUT FILE> <COLUMN INDEX>
+##Pvalue-FDR-stdout.sh <INPUT FILE> <COLUMN INDEX> <NUMBER OF TESTS>
 
 ############################################################################
 ##ACTIONS:
@@ -23,9 +24,12 @@
 ##Input.
 INPUTFILE=$(readlink -f $1)
 PVALCOL=$2
+NTESTS=$3
 
-#Run R to perform FDR correction.
-Rscript --vanilla -e "
+if [[ -z "${NTESTS}" ]] ; then
+
+	#Run R to perform FDR correction.
+	Rscript --vanilla -e "
 #Read input data.
 DATA<-read.table('${INPUTFILE}',header=TRUE,sep='\t',stringsAsFactors=FALSE)
 
@@ -38,3 +42,25 @@ FDRPVALUES<-p.adjust(PVALUES,method='fdr')
 #Print FDR p-values.
 cat(FDRPVALUES,sep='\n')
 "
+
+else
+
+    #Run R to perform FDR correction.
+	Rscript --vanilla -e "
+#Read input data.
+DATA<-read.table('${INPUTFILE}',header=TRUE,sep='\t',stringsAsFactors=FALSE)
+
+#Extract p-values.
+PVALUES<-as.numeric(DATA[,${PVALCOL}])
+
+#Total number of tests.
+TOTALNTESTS<-as.numeric(${NTESTS})
+
+#Perform FDR correction.
+FDRPVALUES<-p.adjust(PVALUES,method='fdr',n=TOTALNTESTS)
+
+#Print FDR p-values.
+cat(FDRPVALUES,sep='\n')
+"
+
+fi
