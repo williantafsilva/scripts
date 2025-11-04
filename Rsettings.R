f0c019cd3456ac6a09b66c35737d3bcac5cf430a
Willian Silva <williantafsilva@users.noreply.github.com>
2025-11-04 14:02:33 +0100

    Update Rsettings.R

--- Rsettings.R
+++ Rsettings.R
@@ -24,6 +24,24 @@ myggplottheme<-theme(title=element_text(size=10,face="bold"),
                      strip.background=element_rect(colour="black",
                                                    fill="white"))
 
+myggplottheme_nolegend<-theme(title=element_text(size=10,face="bold"),
+                              axis.title=element_text(size=10,face="bold"),
+                              axis.text=element_text(size=10),
+                              axis.text.x=element_text(angle=60,size=8,vjust=0.5),
+                              legend.position="none",
+                              legend.title=element_text(size=10,face="bold"),
+                              legend.text=element_text(size=10),
+                              legend.key=element_blank(),
+                              panel.grid=element_line(colour="gray90"),
+                              #panel.grid.major.x=element_blank(),
+                              #panel.grid.minor.x=element_blank(),
+                              panel.background=element_rect(fill="white",colour="black"),
+                              #panel.grid.major=element_blank(),
+                              #panel.grid.minor=element_blank(),
+                              strip.background=element_rect(colour="black",
+                                                            fill="white"))
+
+
 #List ggplot2 colors.
 ggplot2colors<-grDevices::colors()
 Rbasecolors<-colors()
