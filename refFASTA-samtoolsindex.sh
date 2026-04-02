3599ef2d2c6c542e6712a554396de88c6eaf1a91
Willian Silva <williantafsilva@users.noreply.github.com>
2026-04-02 14:35:09 +0200

    Create refFASTA-samtoolsindex.sh

---
+++ refFASTA-samtoolsindex.sh
@@ -0,0 +1,115 @@
+#!/bin/bash -l
+##Add -l to the shebang to inherit bash profile variables and configuration.
+##Required environment variables: ${PATHTOMYSUBMITTEDSCRIPTS}, ${PATHTOMYSLURM}, ${PATHTOPROJTMP}, ${PROJECT_ID}, ${MYSLURMFILE}, ${MYEMAIL}.
+############################################################################
+################################## SCRIPT ##################################
+############################################################################
+##Author: Willian T.A.F. Silva (willian.silva@evobiolab.com).
+############################################################################
+##SCRIPT DESCRIPTION:
+
+##Description:
+##Create index file of reference genome FASTA file using Samtools.
+
+##Input $1: Output location.
+##Input $2: FASTA reference genome (.fasta, .fa, .fna).
+##Output: Samtools (faidx) index file (.fai).
+
+##Usage: 
+##sbatch \
+##    -A ${PROJECT_ID} \
+##    -o ${MYSLURMFILE} \
+##    -p shared \
+##    -N 1 \
+##    -n 10 \
+##    -t 05-00:00:00 \
+##    --mail-type=ALL \
+##    --mail-user=${MYEMAIL} \
+##    -J refFASTA-samtoolsindex-${<INPUTFILE>##*/} \
+##    refFASTA-samtoolsindex.sh <OUTPUT LOCATION> <REFERENCE GENOME FASTA FILE>
+
+############################################################################
+##SCRIPT NAME:
+
+SCRIPTNAME=$(echo "refFASTA-samtoolsindex.sh") 
+
+############################################################################
+##JOB ID:
+
+RUNDATE=$(date +"%Y%m%d%H%M%S")
+if [[ -z "${SLURM_JOB_ID}" ]] ; then JOBID=${RUNDATE} ; else JOBID=${SLURM_JOB_ID} ; fi 
+
+############################################################################
+##SUBMITTED SCRIPT COPY:
+
+cat $0 > $(echo "${PATHTOMYSUBMITTEDSCRIPTS}/job${JOBID}-date${RUNDATE}-${SCRIPTNAME}") 
+
+############################################################################
+##DIAGNOSTICS:
+
+echo "$(date +"%Y-%m-%d @ %H:%M:%S"): 
+Submission: ${SCRIPTNAME} $@
+User (\$USER): ${USER}
+Host name (hostname -f): $(hostname -f)
+Operating system: $(uname -a)
+PATH: ${PATH}
+Job ID (\$SLURM_JOB_ID): ${SLURM_JOB_ID}"
+
+############################################################################
+##LOAD TOOLS:
+
+module load bioinfo-tools
+module load bcftools
+module load samtools
+
+############################################################################
+##INPUT:
+
+OUTPUTLOCATION=$(readlink -f $1)
+INPUTFILE=$(readlink -f $2)
+
+INPUTFILELOCATION=${INPUTFILE%/*}
+INPUTFILENAME=${INPUTFILE##*/}
+
+############################################################################
+##OUTPUT:
+
+OUTPUTFILEPREFIX=$(echo ${INPUTFILENAME} | sed 's/\.fasta.*$//' | sed 's/\.fa.*$//' | sed 's/\.fna.*$//' | sed 's/-job[0-9].*$//')
+OUTPUTFILENAME=$(echo "${OUTPUTFILEPREFIX}.samtoolsindex-job${JOBID}.fai") 
+OUTPUTFILE=$(echo "${OUTPUTLOCATION}/${OUTPUTFILENAME}")
+
+############################################################################
+##ACTIONS:
+
+#Index reference genome FASTA file. 
+samtools faidx --fai-idx ${OUTPUTFILE} ${INPUTFILE}
+
+############################################################################
+##SAVE CONTROL FILES:
+
+if [[ -s "${OUTPUTFILE}" ]]; then
+	
+	##README LOG ENTRY:
+echo "############################################################################
+Date: ${RUNDATE}
+Job ID: ${JOBID}
+Script: ${PATHTOMYSUBMITTEDSCRIPTS}/job${JOBID}-date${RUNDATE}-${SCRIPTNAME}
+Input file: ${INPUTFILE}
+Output file: ${OUTPUTFILE}
+" >> $(echo "${OUTPUTLOCATION}/README.txt") 
+
+	##COPY OF SCRIPT:
+	cat $0 > $(echo "${OUTPUTLOCATION}/job${JOBID}.sh")
+	##COPY OF SLURM FILE:
+	cat $(echo "${PATHTOMYSLURM}/slurm-${JOBID}.out") > $(echo "${OUTPUTLOCATION}/job${JOBID}.out") 
+
+else
+
+	##COPY OF SCRIPT:
+	cat $0 > $(echo "${OUTPUTLOCATION}/job${JOBID}.sh")
+	##COPY OF SLURM FILE:
+	cat $(echo "${PATHTOMYSLURM}/slurm-${JOBID}.out") > $(echo "${OUTPUTLOCATION}/job${JOBID}.err") 
+
+fi
+
+exit 0
