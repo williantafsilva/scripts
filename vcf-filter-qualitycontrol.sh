8b90db48e7335f19b10ebb877c9d5e49f4cd4e84
Willian Silva <williantafsilva@users.noreply.github.com>
2026-04-07 16:37:17 +0200

    Create vcf-filter-qualitycontrol.sh

---
+++ vcf-filter-qualitycontrol.sh
@@ -0,0 +1,123 @@
+#!/bin/bash -l
+##Add -l to the shebang to inherit bash profile variables and configuration.
+##Required environment variables: ${PATHTOMYSUBMITTEDSCRIPTS}, ${PATHTOMYSLURM}, ${PATHTOPROJTMP}, ${PROJECT_ID}, ${MYSLURMFILE}, ${MYEMAIL}.
+############################################################################
+################################## SCRIPT ##################################
+############################################################################
+##Author: Willian T.A.F. Silva (willian.silva@evobiolab.com).
+############################################################################
+##SCRIPT DESCRIPTION:
+
+##Description:
+##Perform basic quality filtering on a VCF file (.vcf.gz).
+
+##Input $1: Output location.
+##Input $2: VCF file (.vcf.gz).
+##Output: VCF file (.vcf.gz) and index file (.tbi).
+
+##Usage: 
+##sbatch \
+##    -A ${PROJECT_ID} \
+##    -o ${MYSLURMFILE} \
+##    -p shared \
+##    -N 1 \
+##    -n 10 \
+##    -t 05-00:00:00 \
+##    --mail-type=ALL \
+##    --mail-user=${MYEMAIL} \
+##    -J vcf-filter-qualitycontrol-${<INPUTFILE>##*/} \
+##    vcf-filter-qualitycontrol.sh <OUTPUT LOCATION> <INPUT VCF FILE>
+
+############################################################################
+##SCRIPT NAME:
+
+SCRIPTNAME=$(echo "vcf-filter-qualitycontrol.sh") 
+
+############################################################################
+##JOB ID:
+
+RUNDATE=$(date +"%Y%m%d%H%M%S")
+if [[ -z "${SLURM_JOB_ID}" ]] ; then JOBID=${RUNDATE} ; else JOBID=${SLURM_JOB_ID} ; fi 
+
+############################################################################
+##SUBMITTED SCRIPT COPY:
+
+cat $0 > $(echo "${PATHTOMYSUBMITTEDSCRIPTS}/job${JOBID}-date${RUNDATE}-${SCRIPTNAME}") 
+
+############################################################################
+##DIAGNOSTICS:
+
+echo "$(date +"%Y-%m-%d @ %H:%M:%S"): 
+Submission: ${SCRIPTNAME} $@
+User (\$USER): ${USER}
+Host name (hostname -f): $(hostname -f)
+Operating system: $(uname -a)
+PATH: ${PATH}
+Job ID (\$SLURM_JOB_ID): ${SLURM_JOB_ID}"
+
+############################################################################
+##LOAD TOOLS:
+
+module load bioinfo-tools
+module load bcftools
+
+############################################################################
+##INPUT:
+
+OUTPUTLOCATION=$(readlink -f $1)
+INPUTFILE=$(readlink -f $2)
+
+INPUTFILELOCATION=${INPUTFILE%/*}
+INPUTFILENAME=${INPUTFILE##*/}
+
+############################################################################
+##OUTPUT:
+
+INPUTFILEPREFIX=$(echo ${INPUTFILENAME} | sed 's/\.vcf$//' | sed 's/\.vcf\.gz$//' | sed 's/\.bcf$//' | sed 's/\.bcf\.gz$//' | sed 's/-job[0-9].*$//')
+OUTPUTFILE1NAME=$(echo "${INPUTFILEPREFIX}.qcfilter-job${JOBID}.vcf.gz") 
+OUTPUTFILE2NAME=$(echo "${OUTPUTFILE1NAME}.tbi") 
+OUTPUTFILE1=$(echo "${OUTPUTLOCATION}/${OUTPUTFILE1NAME}")
+OUTPUTFILE2=$(echo "${OUTPUTLOCATION}/${OUTPUTFILE2NAME}")
+
+############################################################################
+##ACTIONS:
+
+#QUAL=Phred-scaled quality score for the assertion made in ALT.
+#INFO/DP=Combined read depth across samples.
+#FMT/DP=Sample-specific read depth at this position.
+#FMT/GQ=Sample-specific genotype quality at this position, encoded as a phred quality.
+
+bcftools filter --include 'QUAL>=30 && INFO/DP>=10' ${INPUTFILE} -Ou | \
+bcftools filter --set-GTs . --exclude 'FMT/DP<10 || FMT/GQ<20' -Ou | \
+bcftools view -f PASS --output-type z --write-index=tbi -output ${OUTPUTFILE1}
+
+############################################################################
+##SAVE CONTROL FILES:
+
+if [[ -s "${OUTPUTFILE1}" ]]; then
+	
+	##README LOG ENTRY:
+echo "############################################################################
+Date: ${RUNDATE}
+Job ID: ${JOBID}
+Script: ${PATHTOMYSUBMITTEDSCRIPTS}/job${JOBID}-date${RUNDATE}-${SCRIPTNAME}
+Input file: ${INPUTFILE}
+Output file: ${OUTPUTFILE1}
+Output file: ${OUTPUTFILE2}
+" >> $(echo "${OUTPUTLOCATION}/README.txt") 
+
+	##COPY OF SCRIPT:
+	cat $0 > $(echo "${OUTPUTLOCATION}/job${JOBID}.sh")
+	##COPY OF SLURM FILE:
+	cat $(echo "${PATHTOMYSLURM}/slurm-${JOBID}.out") > $(echo "${OUTPUTLOCATION}/job${JOBID}.out") 
+
+else
+
+	##COPY OF SCRIPT:
+	cat $0 > $(echo "${OUTPUTLOCATION}/job${JOBID}.sh")
+	##COPY OF SLURM FILE:
+	cat $(echo "${PATHTOMYSLURM}/slurm-${JOBID}.out") > $(echo "${OUTPUTLOCATION}/job${JOBID}.err") 
+
+fi
+
+exit 0
