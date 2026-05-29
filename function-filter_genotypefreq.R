5b19398d112d74f8174f9395d58a241c62a3a861
Willian Silva <williantafsilva@users.noreply.github.com>
2026-05-29 07:19:02 +0200

    Update function-filter_genotypefreq.R

--- function-filter_genotypefreq.R
+++ function-filter_genotypefreq.R
@@ -11,9 +11,9 @@ filter_genotypefreq<-function(MATRIX_GENOTYPES, #Data frame of genotypes with SN
                               OUTPUTGENCOUNT=FALSE){  #If true, the function will output a list containing, in addition to the SNP IDs of low frequency genotypes, the table of genotype counts.
   
   #Load libraries.
-  library(ggplot2)
+  #library(ggplot2)
   library(tidyverse)
-  library(gtools)
+  #library(gtools)
   
   #Transform phased genotypes into unphased genotypes.
   DATA_GENOTYPES<-data.frame(lapply(MATRIX_GENOTYPES,function(x){
@@ -74,5 +74,88 @@ filter_genotypefreq<-function(MATRIX_GENOTYPES, #Data frame of genotypes with SN
   return(OUTPUT)
   
 } 
+
+filter_genotypefreq_DT<-function(MATRIX_GENOTYPES, #Data frame of genotypes with SNPs per row (1st column is SNP ID) and samples per column.
+                                 MINGENFREQ=0, #Minimum genotype frequency (<1) or count (>1).
+                                 OUTPUTFILTEREDMATRIX=FALSE, #If true, the function will output a list containing, in addition to the SNP IDs of low frequency genotypes, the filtered genotype matrix.
+                                 OUTPUTLOWFREQGENOTYPES=FALSE, #If true, the function will output a list containing, in addition to the SNP IDs of low frequency genotypes, the genotype matrix of SNPs with low frequency genotypes.
+                                 OUTPUTGENCOUNT=FALSE){  #If true, the function will output a list containing, in addition to the SNP IDs of low frequency genotypes, the table of genotype counts.
+  
+  #Load libraries.
+  library(tidyverse)
+  library(data.table)
+
+  #Make sure MATRIX_GENOTYPES is a data.table object.
+  setDT(MATRIX_GENOTYPES)
+
+  #Function to transform phased genotypes into unphased genotypes.
+  UNPHASEGENOTYPE<-function(x){
+    x<-gsub("\\|","/",x)
+    x<-gsub("C/A","A/C",x)
+    x<-gsub("G/A","A/G",x)
+    x<-gsub("T/A","A/T",x)
+    x<-gsub("G/C","C/G",x)
+    x<-gsub("T/C","C/T",x)
+    gsub("T/G","G/T",x)}
+
+  #Apply function to MATRIX_GENOTYPES.
+  MATRIX_GENOTYPES[,(names(MATRIX_GENOTYPES)):=lapply(.SD,UNPHASEGENOTYPE)]
+  setDT(MATRIX_GENOTYPES)
+  
+  #Define the genotypes to be counted.
+  GENOTYPES<-c(AA="A/A",AC="A/C",AG="A/G",AT="A/T", 
+               CC="C/C",CG="C/G",CT="C/T", 
+               GG="G/G",GT="G/T",
+               TT="T/T", 
+               MISSING="./.")
+  
+  #Create the counts table.
+  GENOTYPES_COUNTS<-data.table(SNP=MATRIX_GENOTYPES[[1]])
+  #Loop through and compute rowSums using .SD (excluding the 1st column).
+  for(GENNAME in names(GENOTYPES)){
+    GENOTYPES_COUNTS[,(GENNAME):=rowSums(MATRIX_GENOTYPES[,.SD,.SDcols=-1]==GENOTYPES[GENNAME])]
+  }
+  
+  #Add the final summary columns by reference.
+  GENOTYPECOLS<-names(GENOTYPES)
+  GENOTYPES_COUNTS[,`:=`(N_Samples=rowSums(.SD),
+                         N_Genotypes=rowSums(.SD>0)),
+                   .SDcols=GENOTYPECOLS]
+  setDT(GENOTYPES_COUNTS)
+
+  if(MINGENFREQ<1){
+    #Calculate row sums in-place using .SD.
+    GENOTYPES_COUNTS[,N_Genotypes_ltMINGENFREQ:=rowSums(.SD>0 & (.SD/N_Samples)<MINGENFREQ),.SDcols=2:12]
+  }else{
+    #Calculate row sums in-place using .SD.
+    GENOTYPES_COUNTS[,N_Genotypes_ltMINGENFREQ:=rowSums(.SD>0 & .SD<MINGENFREQ),.SDcols=2:12]
+  }
+  #SNPs that have at least one genotype with frequency <MINGENFREQ.
+  SNPltMINGENFREQ<-GENOTYPES_COUNTS[N_Genotypes_ltMINGENFREQ>0,SNP]
+
+  #Initialize output list.
+  OUTPUT<-list(SNPltMINGENFREQ=SNPltMINGENFREQ)
+  
+  #Set key on the first column of DATA_GENOTYPES for blistering fast filtering
+  SNPCOLNAME<-names(MATRIX_GENOTYPES)[1]
+  setkeyv(MATRIX_GENOTYPES,SNPCOLNAME)
+  
+  if(OUTPUTFILTEREDMATRIX){
+    #Filter out SNPs that have at least one genotype with frequency <MINGENFREQ.
+    DATA_FILTEREDGENOTYPES<-MATRIX_GENOTYPES[!.(SNPltMINGENFREQ)]
+    OUTPUT<-c(OUTPUT,list(MATRIXFILTERED=DATA_FILTEREDGENOTYPES))
+  }
+  if(OUTPUTLOWFREQGENOTYPES){
+    #Filter out SNPs that have at least one genotype with frequency <MINGENFREQ.
+    DATA_GENOTYPESLOWFREQ<-MATRIX_GENOTYPES[.(SNPltMINGENFREQ)]
+    OUTPUT<-c(OUTPUT,list(MATRIXLOWFREQ=DATA_GENOTYPESLOWFREQ))
+  }
+  if(OUTPUTGENCOUNT){
+    OUTPUT<-c(OUTPUT,list(COUNTS=GENOTYPES_COUNTS))
+  }
+  
+  return(OUTPUT)
+  
+}
   
   
